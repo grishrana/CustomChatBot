fc96803f8fdea12cff867006967f67fa71cfe86d
Grish Rana <grishrana6@gmail.com>
2025-05-21 22:06:31 +0545

    Feat: Added a chat endpoint and a Database(prompt and prompt_date) setup

--- ChatBot/ChatBot/settings.py
+++ ChatBot/ChatBot/settings.py
@@ -34,6 +34,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "chat.apps.ChatConfig",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
@@ -107,7 +108,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = "UTC"
+TIME_ZONE = "Asia/Kathmandu"
 
 USE_I18N = True
 
--- ChatBot/chat/models.py
+++ ChatBot/chat/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.utils import timezone
+
 
 # Create your models here.
+class Prompts(models.Model):
+    def __str__(self):
+        return self.prompt
+
+    prompt = models.CharField(max_length=500)
+    prompt_date = models.DateTimeField("date_prompted", default=timezone.now())
